cf3d374e6ed07c79a3a26f5e5cdc4938cea83af4
JolitaAG <jolita278@gmail.com>
2017-06-08 11:10:47 +0300

    created locale translations files

---
+++ resources/lang/en/app.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    "admin_side_bar" => "Admin menu",
+    "pages" => "Pages",
+    "categories" => "Categories",
+    "resources" => "Resources",
+    "orders" => "Orders",
+    "users" => "Users",
+    "languages" => "Languages",
+    "menus" =>"Menus",
+    "is_active"=> "Is active",
+
+
+    "inspired_by" => "Inspired by:",
+    "main_title" => "Discover VR",
+    "banner-text" => "See the world with different eyes. Virtual reality experience exhibition",
+
+
+
+
+
+
+
+
+
+
+
+
+];
---
+++ resources/lang/lt/app.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    "admin_side_bar" => "Admino meniu",
+    "pages" => "Puslapiai",
+    "categories" => "Kategorijos",
+    "resources" => "Failai",
+    "orders" => "Užsakymai",
+    "users" => "Vartotojai",
+    "languages" => "Kalbos",
+    "menus" =>"Meniu",
+    "is_active"=> "Yra aktyvus",
+
+
+    "inspired_by" => "Parodą įkvepia:",
+    "main_title" => "Atrask VR",
+    "banner-text" => "Pamatyk pasaulį kitomis akimis. Virtualios realybės patirčių paroda!",
+
+
+
+
+
+
+
+
+];
