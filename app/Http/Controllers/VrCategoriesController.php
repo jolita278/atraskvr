1b2ce21cf8349b4c95d51bf11096e8544d9c310a
JolitaAG <jolita278@gmail.com>
2017-06-12 15:33:04 +0300

    added create function to categories controller

--- app/Http/Controllers/VrCategoriesController.php
+++ app/Http/Controllers/VrCategoriesController.php
@@ -1,95 +1,111 @@
 <?php namespace App\Http\Controllers;
 
 use App\Models\VrCategories;
+use App\Models\VrLanguageCodes;
 use Illuminate\Routing\Controller;
 use Ramsey\Uuid\Uuid;
 
-class VrCategoriesController extends Controller {
+class VrCategoriesController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /vrcategories
-	 *
-	 * @return Response
+    /**
+     * Display a listing of the resource.
+     * GET /vrcategories
      *
-	 */
-	public function adminIndex()
-	{
+     * @return Response
+     *
+     */
+    public function adminIndex()
+    {
         $configuration ['title'] = trans('app.categories_list');
         $configuration ['list'] = VrCategories::get()->toArray();
         $configuration ['new'] = url('admin/categories/create');
         return view('admin.adminList', $configuration);
-	}
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /vrcategories/create
-	 *
-	 * @return Response
-	 */
-	public function adminCreate()
-	{
+    /**
+     * Show the form for creating a new resource.
+     * GET /vrcategories/create
+     *
+     * @return Response
+     */
+    public function adminCreate()
+    {
+        $configuration['fields'][] = [
+            "type" => "drop_down",
+            "key" => "language_code",
+            "options" => getActiveLanguage(),
+            "label" => trans('app.languages')
+        ];
+        $configuration['fields'][] = [
+            "type" => "single_line",
+            "key" => "name",
+            "label" => trans('app.name')
+        ];
+        $configuration ['title'] = trans('app.categories');
+        $configuration ['url'] = url('admin/categories/create');
 
-	}
+        return view('admin.adminForm', $configuration);
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /vrcategories
-	 *
-	 * @return Response
-	 */
-	public function adminStore()
-	{
-	    //
-	}
-
-	/**
-	 * Display the specified resource.
-	 * GET /vrcategories/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function adminShow($id)
-	{
+    /**
+     * Store a newly created resource in storage.
+     * POST /vrcategories
+     *
+     * @return Response
+     */
+    public function adminStore()
+    {
         //
-	}
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /vrcategories/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function adminEdit($id)
-	{
-       //
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /vrcategories/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function adminUpdate($id)
-	{
-       //
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /vrcategories/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function adminDestroy($id)
-	{
+    /**
+     * Display the specified resource.
+     * GET /vrcategories/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function adminShow($id)
+    {
         //
-	}
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * GET /vrcategories/{id}/edit
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function adminEdit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * PUT /vrcategories/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function adminUpdate($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /vrcategories/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function adminDestroy($id)
+    {
+        //
+    }
 
 
 }
